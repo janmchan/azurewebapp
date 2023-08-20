2cb3409834dcd461831e29f6effc302b9ecfedfd
Michael Chan <git.python407@slmails.com>
2023-08-20 22:29:40 +1200

    Show claims

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -17,25 +17,4 @@ public class IndexModel : PageModel
     {
 
     }
-    private string? authMessage;
-    private string? surname;
-    private IEnumerable<Claim> claims = Enumerable.Empty<Claim>();
-
-    private async Task GetClaimsPrincipalData()
-    {
-        var authState = await AuthenticationStateProvider
-            .GetAuthenticationStateAsync();
-        var user = authState.User;
-
-        if (user.Identity is not null && user.Identity.IsAuthenticated)
-        {
-            authMessage = $"{user.Identity.Name} is authenticated.";
-            claims = user.Claims;
-            surname = user.FindFirst(c => c.Type == ClaimTypes.Surname)?.Value;
-        }
-        else
-        {
-            authMessage = "The user is NOT authenticated.";
-        }
-    }
 }
